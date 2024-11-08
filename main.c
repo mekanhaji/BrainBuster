ff342a28bb46c9eca0f6ca497ad14361277faf29
kanhaji <myself.kanhaji@gmail.com>
2024-11-08 20:13:11 +0530

    fix: play again functionality chore: improve interface feat: re reader the screen after each question

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #include "windows.h"
 
 int ask_question(const struct Question* q) {
-    printf("%s\n", q->question);
+    printf("Q > %s\n", q->question);
     for (int i = 0; i < q->option_count; i++) {
         printf("%d. %s\n", i + 1, q->options[i]);
     }
@@ -38,6 +38,7 @@ int ask_question(const struct Question* q) {
 }
 
 void show_title() {
+    printf("\n\n");
     printf("\t /$$$$$$$                     /$$                 /$$$$$$$                        /$$                        \n");
     printf("\t| $$__  $$                   |__/                | $$__  $$                      | $$                        \n");
     printf("\t| $$  \\ $$  /$$$$$$  /$$$$$$  /$$ /$$$$$$$       | $$  \\ $$ /$$   /$$  /$$$$$$$ /$$$$$$    /$$$$$$   /$$$$$$ \n");
@@ -46,16 +47,27 @@ void show_title() {
     printf("\t| $$  \\ $$| $$      /$$__  $$| $$| $$  | $$      | $$  \\ $$| $$  | $$ \\____  $$  | $$ /$$| $$_____/| $$      \n");
     printf("\t| $$$$$$$/| $$     |  $$$$$$$| $$| $$  | $$      | $$$$$$$/|  $$$$$$/ /$$$$$$$/  |  $$$$/|  $$$$$$$| $$      \n");
     printf("\t|_______/ |__/      \\_______/|__/|__/  |__/      |_______/  \\______/ |_______/    \\___/   \\_______/|__/      \n");
+    printf("\n\n");
 }
 
-int main() {
+void welcome_message() {
+    printf("%s%sWelcome to the Quiz Game!%s\n", BOLD, GREEN, RESET);
+    printf("You will be asked 5 random questions.\n\n");
+}
+
+void game() {
+    clear_screen();
     srand(time(NULL));
+
     int max_score = 0;
     int num_questions = 5;  // Number of questions to ask
+
     show_title();
-    printf("%s%sWelcome to the Quiz Game!%s\n", BOLD, GREEN, RESET);
-    printf("You will be asked %d random questions.\n\n", num_questions);
+    welcome_message();
+
     int asked[QUESTION_COUNT] = { 0 };  // To keep track of asked questions
+    char play_again;
+
     do
     {
         int score = 0;
@@ -67,19 +79,33 @@ int main() {
 
             asked[index] = 1;
             printf("Current Score: %d\n", score);
+            printf("You have answered %d out of %d\n\n", i, num_questions);
             int answer = ask_question(&questions[index]);
             score += answer;
+            printf("Press any key to continue \n");
+            getchar();
+
+            clear_screen();
+            show_title();
         }
 
         if (score > max_score) {
             max_score = score;
         }
+
         printf("Quiz completed! Your score is: %d\n", score);
-        printf("Do you want to play again? (y/n): ");
-    } while (getchar() != '\n');
+        printf("Enter `X` to exit or any other key to play again: ");
+        play_again = getchar();
+
+        clear_screen();
+        show_title();
+    } while (play_again != 'x' || play_again != 'X');
 
 
     printf("Quiz completed!\n");
+}
 
+int main() {
+    game();
     return 0;
 }
