652b7c462b377aba63e893990dca9d44310a6142
kanhaji <myself.kanhaji@gmail.com>
2024-11-08 15:55:26 +0530

    feat: added scoring system

--- lib/utility.h
+++ lib/utility.h
@@ -1,6 +1,12 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 
+#define RESET   "\033[0m"
+#define BOLD    "\033[1m"
+#define UNDERLINE "\033[4m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+
 void clear_input_buffer();
 
 #endif // UTILITY_H
